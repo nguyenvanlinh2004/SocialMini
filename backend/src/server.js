8c729884a79a289965a927e7e226a1c2dc35756e
nguyenvanlinh <vanlinhdz2004@gmail.com>
2025-11-14 14:03:46 +0700

    haha

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,12 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // cors
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 // middlewarees
 app.use(express.json());
 app.use(cookieParser());
